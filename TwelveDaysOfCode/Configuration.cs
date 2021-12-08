111cc17b4687ed6d8483e946a62bc8c88a333c62
Craig Hawker <craig.hawker@m-files.com>
2021-12-08 13:22:09 +0000

    Initial implementation for day 1

---
+++ TwelveDaysOfCode/Configuration.cs
@@ -0,0 +1,13 @@
+using MFiles.VAF.Configuration;
+using MFiles.VAF.Configuration.JsonAdaptor;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace TwelveDaysOfCode
+{
+    [DataContract]
+    public class Configuration
+    {
+    }
+}
---
+++ TwelveDaysOfCode/VaultApplication.cs
@@ -0,0 +1,20 @@
+using MFiles.VAF;
+using MFiles.VAF.AppTasks;
+using MFiles.VAF.Common;
+using MFiles.VAF.Configuration;
+using MFiles.VAF.Core;
+using MFilesAPI;
+using System;
+using System.Diagnostics;
+
+namespace TwelveDaysOfCode
+{
+    /// <summary>
+    /// The entry point for this Vault Application Framework application.
+    /// </summary>
+    /// <remarks>Examples and further information available on the developer portal: http://developer.m-files.com/. </remarks>
+    public class VaultApplication
+        : MFiles.VAF.Extensions.ConfigurableVaultApplicationBase<Configuration>
+    {
+    }
+}
